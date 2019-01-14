e3cb05c3a409879bdd05c385f7547f91d705b528
Daniel Egger <daniel@eggers-club.de>
2019-01-14 01:10:33 +0100

    Adapted to latest stm32f0xx-hal crate and bumped version
    Signed-off-by: Daniel Egger <daniel@eggers-club.de>

--- src/main.rs
+++ src/main.rs
@@ -30,109 +30,110 @@ use core::fmt::Write;
 
 #[entry]
 fn main() -> ! {
-    if let (Some(p), Some(cp)) = (stm32::Peripherals::take(), Peripherals::take()) {
-        let gpioa = p.GPIOA.split();
-        let gpiof = p.GPIOF.split();
+    if let (Some(mut p), Some(cp)) = (stm32::Peripherals::take(), Peripherals::take()) {
+        cortex_m::interrupt::free(|cs| {
+            let mut rcc = p.RCC.configure().sysclk(48.mhz()).freeze(&mut p.FLASH);
 
-        let rcc = p.RCC.constrain();
-        let clocks = rcc.cfgr.sysclk(48.mhz()).freeze();
+            let gpioa = p.GPIOA.split(&mut rcc);
+            let gpiof = p.GPIOF.split(&mut rcc);
 
-        // Get delay provider
-        let mut delay = Delay::new(cp.SYST, clocks);
+            // Get delay provider
+            let mut delay = Delay::new(cp.SYST, &rcc);
 
-        let mut led = gpioa.pa1.into_push_pull_output();
-        led.set_high();
-        delay.delay_ms(300_u16);
-        led.set_low();
-        delay.delay_ms(300_u16);
-        led.set_high();
-
-        // Disable the watchdog when the cpu is stopped under debug
-        p.DBGMCU.apb1_fz.modify(|_, w| w.dbg_iwdg_stop().set_bit());
-
-        let mut watchdog = Watchdog::new(p.IWDG);
-        watchdog.start(Hertz(1));
-
-        let scl = gpiof
-            .pf1
-            .into_alternate_af1()
-            .internal_pull_up(true)
-            .set_open_drain();
-        let sda = gpiof
-            .pf0
-            .into_alternate_af1()
-            .internal_pull_up(true)
-            .set_open_drain();
-
-        // Setup I2C1
-        let i2c = I2c::i2c1(p.I2C1, (scl, sda), 400.khz());
-
-        let i2c_bus = shared_bus::CortexMBusManager::new(i2c);
-
-        let mut disp: GraphicsMode<_> = Builder::new().connect_i2c(i2c_bus.acquire()).into();
-
-        disp.init().unwrap();
-        disp.flush().unwrap();
-
-        let mut ina260 = INA260::new(i2c_bus.acquire(), 0x40).unwrap();
-
-        // Slow down sampling a bit for more accuracy
-        ina260
-            .set_bvconvtime_mode(ina260::BVConvTime::MS4_156)
-            .unwrap();
-        ina260
-            .set_scconvtime_mode(ina260::SCConvTime::MS4_156)
-            .unwrap();
-        ina260.set_averaging_mode(ina260::Averaging::AVG16).unwrap();
-
-        // Endless loop
-        loop {
-            led.set_low();
-
-            // Read voltage
-            {
-                let (major, minor) = ina260.voltage_split().unwrap();
-                let mut v: String<U10> = String::new();
-                write!(v, "{:3}.{:05}V", major, minor).unwrap();
-                disp.draw(
-                    Font12x16::render_str(v.as_str())
-                        .with_stroke(Some(1u8.into()))
-                        .into_iter(),
-                );
-            }
-
-            // Read current
-            {
-                let (major, minor) = ina260.current_split().unwrap();
-                let mut v: String<U10> = String::new();
-                write!(v, "{:3}.{:05}A", major, minor).unwrap();
-                disp.draw(
-                    Font12x16::render_str(v.as_str())
-                        .with_stroke(Some(1u8.into()))
-                        .translate(Coord::new(0, 16))
-                        .into_iter(),
-                );
-            }
-
-            // Read power
-            {
-                let (major, minor) = ina260.power_split().unwrap();
-                let mut v: String<U10> = String::new();
-                write!(v, "{:3}.{:05}W", major, minor).unwrap();
-                disp.draw(
-                    Font12x16::render_str(v.as_str())
-                        .with_stroke(Some(1u8.into()))
-                        .translate(Coord::new(0, 32))
-                        .into_iter(),
-                );
-            }
-
+            let mut led = gpioa.pa1.into_push_pull_output(cs);
             led.set_high();
+            delay.delay_ms(300_u16);
+            led.set_low();
+            delay.delay_ms(300_u16);
+            led.set_high();
+
+            // Disable the watchdog when the cpu is stopped under debug
+            p.DBGMCU.apb1_fz.modify(|_, w| w.dbg_iwdg_stop().set_bit());
+
+            let mut watchdog = Watchdog::new(p.IWDG);
+            watchdog.start(Hertz(1));
+
+            let scl = gpiof
+                .pf1
+                .into_alternate_af1(cs)
+                .internal_pull_up(cs, true)
+                .set_open_drain(cs);
+            let sda = gpiof
+                .pf0
+                .into_alternate_af1(cs)
+                .internal_pull_up(cs, true)
+                .set_open_drain(cs);
+
+            // Setup I2C1
+            let i2c = I2c::i2c1(p.I2C1, (scl, sda), 400.khz(), &mut rcc);
+
+            let i2c_bus = shared_bus::CortexMBusManager::new(i2c);
+
+            let mut disp: GraphicsMode<_> = Builder::new().connect_i2c(i2c_bus.acquire()).into();
+
+            disp.init().unwrap();
             disp.flush().unwrap();
 
-            // Reset watchdog
-            watchdog.feed();
-        }
+            let mut ina260 = INA260::new(i2c_bus.acquire(), 0x40).unwrap();
+
+            // Slow down sampling a bit for more accuracy
+            ina260
+                .set_bvconvtime_mode(ina260::BVConvTime::MS4_156)
+                .unwrap();
+            ina260
+                .set_scconvtime_mode(ina260::SCConvTime::MS4_156)
+                .unwrap();
+            ina260.set_averaging_mode(ina260::Averaging::AVG16).unwrap();
+
+            // Endless loop
+            loop {
+                led.set_low();
+
+                // Read voltage
+                {
+                    let (major, minor) = ina260.voltage_split().unwrap();
+                    let mut v: String<U10> = String::new();
+                    write!(v, "{:3}.{:05}V", major, minor).unwrap();
+                    disp.draw(
+                        Font12x16::render_str(v.as_str())
+                            .with_stroke(Some(1u8.into()))
+                            .into_iter(),
+                    );
+                }
+
+                // Read current
+                {
+                    let (major, minor) = ina260.current_split().unwrap();
+                    let mut v: String<U10> = String::new();
+                    write!(v, "{:3}.{:05}A", major, minor).unwrap();
+                    disp.draw(
+                        Font12x16::render_str(v.as_str())
+                            .with_stroke(Some(1u8.into()))
+                            .translate(Coord::new(0, 16))
+                            .into_iter(),
+                    );
+                }
+
+                // Read power
+                {
+                    let (major, minor) = ina260.power_split().unwrap();
+                    let mut v: String<U10> = String::new();
+                    write!(v, "{:3}.{:05}W", major, minor).unwrap();
+                    disp.draw(
+                        Font12x16::render_str(v.as_str())
+                            .with_stroke(Some(1u8.into()))
+                            .translate(Coord::new(0, 32))
+                            .into_iter(),
+                    );
+                }
+
+                led.set_high();
+                disp.flush().unwrap();
+
+                // Reset watchdog
+                watchdog.feed();
+            }
+        });
     }
 
     loop {
